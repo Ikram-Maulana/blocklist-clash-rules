1ed9f729345770032be157f8829431ee2a1ea881
Ikram Maulana <ikram075@ummi.ac.id>
2024-06-09 20:19:56 +0700

    feat(global): big update projects
    - change project name
    - update readme.md
    - update index.ts
    - update image
    - update github actions script
    - add new rule_provider file

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-# OISD Rule Providers for Clash
+# Blocklist Rule Providers for Clash
 
-This repository contains a set of scripts to generate Clash rule providers for the [OISD blocklist](https://oisd.nl/). The OISD blocklist is a list of domains that are known to be used for serving ads, tracking, and malware. The currently available rules can be [found here](https://github.com/ikram-maulana/oisd-clash-rule/tree/main/rule_provider).
+This repository contains a set of scripts to generate Clash rule providers. The rules are generated from various blocklist sources. Used to split the traffic connection based on the blocklist category.
+
+## Available Rule Providers
+
+- [Blocklist_Ads_D3ward](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Ads_D3ward.yaml)
+- [Blocklist_Ads_OISD](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Ads.yaml)
+- [Blocklist_NSFW](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_NSFW.yaml)
+- [Blocklist_Ads_Indo](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Ads_Indo.yaml)
+- [Blocklist_Ads_MultiPro](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Ads_MultiPro.yaml)
+- [Blocklist_DohVpnProxyBypass](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_DohVpnProxyBypass.yaml)
+- [Blocklist_Native_Apple](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_Apple.yaml)
+- [Blocklist_Native_OppoRealme](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_OppoRealme.yaml)
+- [Blocklist_Native_TikTok_Extended](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_TikTok_Extended.yaml)
+- [Blocklist_Native_TikTok](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_TikTok.yaml)
+- [Blocklist_Native_Vivo](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_Vivo.yaml)
+- [Blocklist_Native_WinOffice](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_WinOffice.yaml)
+- [Blocklist_Native_Xiaomi](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Native_Xiaomi.yaml)
+- [Blocklist_NoSafeSearch](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_NoSafeSearch.yaml)
+- [Blocklist_Threat_Medium](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Threat_Medium.yaml)
+- [Blocklist_WindowsSpy](https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_WindowsSpy.yaml)
 
 ## Usage
 
 - Open your `config.yaml` file on your Clash configuration directory.
 
-- Add the following configuration to your `config.yaml` file:
+- Add the following configuration to your `config.yaml` file like the example below:
 
   ```yaml
   rule-providers:
@@ -16,7 +35,7 @@ This repository contains a set of scripts to generate Clash rule providers for t
       path: './rule_provider/Blocklist_Ads_D3ward.yaml'
       url: https://raw.githubusercontent.com/ikram-maulana/oisd-clash-rule/main/rule_provider/Blocklist_Ads_D3ward.yaml
       interval: 86400 # Update rules every 24 hours
-    Blocklist_Ads:
+    Blocklist_Ads_OISD:
       type: http
       behavior: classical
       path: './rule_provider/Blocklist_Ads.yaml'
@@ -37,8 +56,30 @@ This repository contains a set of scripts to generate Clash rule providers for t
     - MATCH,GLOBAL
   ```
 
-- Open `OpenClash -> Plugin Settings -> Operation Mode`, and set the value like image below:
+- Open `OpenClash -> Plugin Settings -> Operation Mode` or `OpenClash -> Global Settings -> Operation Mode`, and set the value like image below:
 
   ![OpenClash Settings](assets/image/openclash-setting.png)
 
 - Restart your Clash instance.
+
+## FAQ
+
+### Can I request a new rule provider?
+
+Yes, you can create an issue on this repository to request a new rule provider.
+
+### How to contribute?
+
+You can create a pull request to add a new rule provider or update the existing rule provider.
+
+### How to update the rule provider?
+
+You can update the rule provider by changing the `interval` value on the rule provider configuration. On OpenClash STB, the rule provider will not be updated automatically. You need to update it manually by clicking the `Update` button on the rule provider configuration.
+
+## Resources
+
+- [ABPindo](https://github.com/ABPindo/indonesianadblockrules)
+- [CrazyMax](https://github.com/crazy-max/WindowsSpyBlocker)
+- [D3ward](https://github.com/d3ward/toolz)
+- [Hagezi](https://github.com/hagezi/dns-blocklists)
+- [OISD](https://oisd.nl/)
--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,168 @@ const __dirname = process.cwd();
 const tempFolder = join(__dirname, 'temp');
 const rulesFolder = join(__dirname, 'rule_provider');
 
-const antiAdsD3wardUrl =
-  'https://raw.githubusercontent.com/d3ward/toolz/master/src/d3host.txt';
-const antiAdsUrl = 'https://big.oisd.nl';
-const antiNSFWUrl = 'https://nsfw.oisd.nl';
+/* The above TypeScript code defines an array `rawUrls` containing objects with properties for URL,
+temporary file name, and processed file name. Each object represents a source URL for raw data
+related to adblock lists or blocklists. The temporary file name is used for storing the downloaded
+data temporarily, and the processed file name is the name of the final processed blocklist file.
+These URLs are likely used for fetching data to create or update various adblock lists in YAML
+format. */
+const rawUrls = [
+  {
+    url: 'https://raw.githubusercontent.com/d3ward/toolz/master/src/d3host.txt',
+    tempFileName: 'd3ward.txt',
+    processedFileName: 'Blocklist_Ads_D3ward.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/aghome.txt',
+    tempFileName: 'abpindo_adblock.txt',
+    processedFileName: 'Blocklist_Ads_Indo.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt',
+    tempFileName: 'hagezi_multi_pro.txt',
+    processedFileName: 'Blocklist_Ads_MultiPro.yaml',
+  },
+  {
+    url: 'https://big.oisd.nl',
+    tempFileName: 'oisd_full_abp.txt',
+    processedFileName: 'Blocklist_Ads_OISD.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/doh-vpn-proxy-bypass.txt',
+    tempFileName: 'hagezi_doh_vpn_proxy_bypass.txt',
+    processedFileName: 'Blocklist_DohVpnProxyBypass.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.apple.txt',
+    tempFileName: 'hagezi_native_apple.txt',
+    processedFileName: 'Blocklist_Native_Apple.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.oppo-realme.txt',
+    tempFileName: 'hagezi_native_oppo_realme.txt',
+    processedFileName: 'Blocklist_Native_OppoRealme.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.tiktok.extended.txt',
+    tempFileName: 'hagezi_native_tiktok_extended.txt',
+    processedFileName: 'Blocklist_Native_TikTok_Extended.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.tiktok.txt',
+    tempFileName: 'hagezi_native_tiktok.txt',
+    processedFileName: 'Blocklist_Native_TikTok.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.vivo.txt',
+    tempFileName: 'hagezi_native_vivo.txt',
+    processedFileName: 'Blocklist_Native_Vivo.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.winoffice.txt',
+    tempFileName: 'hagezi_native_winoffice.txt',
+    processedFileName: 'Blocklist_Native_WinOffice.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.xiaomi.txt',
+    tempFileName: 'hagezi_native_xiaomi.txt',
+    processedFileName: 'Blocklist_Native_Xiaomi.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nosafesearch.txt',
+    tempFileName: 'hagezi_nosafesearch.txt',
+    processedFileName: 'Blocklist_NoSafeSearch.yaml',
+  },
+  {
+    url: 'https://nsfw.oisd.nl',
+    tempFileName: 'oisd_nsfw_abp.txt',
+    processedFileName: 'Blocklist_NSFW.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.medium.txt',
+    tempFileName: 'hagezi_tif_medium.txt',
+    processedFileName: 'Blocklist_Threat_Medium.yaml',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt',
+    tempFileName: 'crazymax_windows_spy.txt',
+    processedFileName: 'Blocklist_WindowsSpy.yaml',
+  },
+];
 
 /**
- * The `download` function asynchronously downloads a file from a specified URL and saves it to a
- * destination using TypeScript.
- * @param {string} url - The `url` parameter is the URL from which you want to download a file.
- * @param {string} dest - The `dest` parameter in the `download` function represents the destination
- * path where the downloaded file will be saved. It should be a string that specifies the full path
- * including the file name where you want to save the downloaded file on your local filesystem.
+ * The function `ensureDirectoryExists` checks if a directory exists and creates it if it doesn't.
+ * @param {string} directory - The `directory` parameter in the `ensureDirectoryExists` function is a
+ * string that represents the path of the directory that needs to be checked and created if it does not
+ * already exist.
  */
-const download = async (url: string, dest: string) => {
+const ensureDirectoryExists = async (directory: string) => {
+  try {
+    await fsPromises.access(directory);
+  } catch {
+    await fsPromises.mkdir(directory);
+  }
+};
+
+/**
+ * The `modifyHeader` function in TypeScript modifies a given text by replacing certain patterns with
+ * 'payload:\n\n'.
+ * @param {string} text - The `modifyHeader` function takes a string `text` as input and modifies it
+ * based on certain patterns.
+ * @returns The `modifyHeader` function takes a string as input and checks for specific patterns using
+ * regular expressions. If a match is found, it replaces the matched text with 'payload:\n\n'.
+ */
+const modifyHeader = (text: string) => {
+  if (text.match(/^(.*?)(?=\|\|)/s)) {
+    return text.replace(/^(.*?)(?=\|\|)/s, 'payload:\n\n');
+  }
+
+  if (
+    text.match(/^(.*?)(?=b(?:(?:https?|ftp):\/\/)?\w[\w-]*(?:\.[\w-]+)+\b)/s)
+  ) {
+    return text.replace(
+      /^(.*?)(?=b(?:(?:https?|ftp):\/\/)?\w[\w-]*(?:\.[\w-]+)+\b)/s,
+      'payload:\n\n',
+    );
+  }
+
+  return text.replace(/^(.*?)(?=0\.0\.0\.0)/s, 'payload:\n\n');
+};
+
+/**
+ * The `modifyDomain` function in TypeScript modifies text by replacing domain names with a specific
+ * format.
+ * @param {string} text - The `modifyDomain` function takes a string `text` as input and performs
+ * certain modifications based on different patterns found in the text.
+ * @returns The `modifyDomain` function takes a string `text` as input and performs different
+ * replacements based on the patterns found in the text.
+ */
+const modifyDomain = (text: string) => {
+  if (text.match(/\|\|([^\^]+)\^/g)) {
+    return text.replace(/\|\|([^\^]+)\^/g, '- DOMAIN,$1');
+  }
+
+  if (text.match(/\b(?:(?:https?|ftp):\/\/)?\w[\w-]*(?:\.[\w-]+)+\b/g)) {
+    return text.replace(
+      /\b(?:(?:https?|ftp):\/\/)?\w[\w-]*(?:\.[\w-]+)+\b/g,
+      '- DOMAIN,$&',
+    );
+  }
+
+  return text.replace(/0\.0\.0\.0\s+([^\s]+)/g, '- DOMAIN,$1');
+};
+
+/**
+ * The `downloadFile` function asynchronously downloads a file from a given URL and saves it to a
+ * specified destination.
+ * @param {string} url - The `url` parameter in the `downloadFile` function is the URL from which the
+ * file needs to be downloaded.
+ * @param {string} dest - The `dest` parameter in the `downloadFile` function represents the
+ * destination path where the downloaded file will be saved on the local filesystem. It should be a
+ * string that specifies the full path including the filename where you want to save the downloaded
+ * file. For example, it could be something like `/path
+ */
+const downloadFile = async (url: string, dest: string) => {
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -39,145 +187,74 @@ const download = async (url: string, dest: string) => {
       reject(new Error('Response body is null'));
     }
   }).catch((error) => {
-    console.error(`Failed to write to file stream: ${error}`);
-    throw error;
+    if (error instanceof Error) {
+      throw new Error(`Failed to save file: ${error.message}`);
+    }
+
+    throw new Error('Failed to save file');
   });
 };
 
 /**
- * The `replaceHeader` function in TypeScript replaces a specific header pattern in a text with a new
- * string.
- * @param {string} text - The `replaceHeader` function takes a string `text` as input and replaces any
- * occurrence of a specific pattern with the text "payload:". The pattern being replaced is a string
- * that starts with "[Adblock Plus]" followed by any characters (including new lines) until the word
- * "Contact:" is
- * @returns The `replaceHeader` function is returning the input `text` with any occurrences of the
- * pattern `/\[Adblock Plus\][\s\S]*?Contact:[^\n]*\n/g` replaced with the string "payload:\n".
- */
-const replaceHeader = (text: string) => {
-  return text.replace(
-    /\[Adblock Plus\][\s\S]*?Contact:[^\n]*\n/g,
-    'payload:\n',
-  );
-};
-
-/**
- * The `replaceHeaderD3ward` function takes a text input and replaces a specific header and creator
- * information with the text "payload:".
- * @param {string} text - The `replaceHeaderD3ward` function takes a string `text` as input and
- * replaces any text that matches the pattern `# Title ... # Created by: d3ward` with the text
- * `'payload:\n'`.
- * @returns The `replaceHeaderD3ward` function takes a string as input and replaces any text that
- * matches the pattern "# Title...# Created by: d3ward" with the text 'payload:\n'. The function
- * returns the modified text after the replacement.
- */
-const replaceHeaderD3ward = (text: string) => {
-  return text.replace(/# Title[\s\S]*?# Created by: d3ward/g, 'payload:');
-};
-
-/**
- * The function `replaceDomain` takes a string as input and replaces occurrences of a specific pattern
- * with a modified version containing "- DOMAIN," prefix.
- * @param {string} text - The `replaceDomain` function takes a string `text` as input and replaces any
- * occurrences of `||` followed by any characters except `^`, and ending with `^` with "- DOMAIN,"
- * followed by the captured characters.
- * @returns The `replaceDomain` function takes a string as input and replaces any occurrences of `||`
- * followed by any characters that are not `^`, and ending with `^` with the string "- DOMAIN,"
- * followed by the captured characters. The modified text is then returned.
- */
-const replaceDomain = (text: string) => {
-  return text.replace(/\|\|([^\^]+)\^/g, '- DOMAIN,$1');
-};
-
-/**
- * The function `replaceDomainD3ward` replaces IP addresses followed by a domain name with a specific
- * format.
- * @param {string} text - The `replaceDomainD3ward` function takes a string `text` as input and
- * replaces any occurrences of a specific pattern in the text. The pattern being replaced is `0.0.0.0`
- * followed by one or more spaces and then a domain name.
- * @returns The function `replaceDomainD3ward` takes a string as input and replaces any occurrences of
- * `0.0.0.0` followed by a space and a domain with `- DOMAIN,` followed by the domain. The modified
- * text is then returned.
- */
-const replaceDomainD3ward = (text: string) => {
-  return text.replace(/0\.0\.0\.0\s+([^\s]+)/g, '- DOMAIN,$1');
-};
-
-/**
- * The function `ensureDirectoryExists` checks if a directory exists and creates it if it doesn't.
- * @param {string} directory - The `directory` parameter in the `ensureDirectoryExists` function is a
- * string that represents the path of the directory that needs to be checked and created if it does not
- * already exist.
- */
-const ensureDirectoryExists = async (directory: string) => {
-  try {
-    await fsPromises.access(directory);
-  } catch {
-    await fsPromises.mkdir(directory);
-  }
-};
-
-/**
- * The function `processFile` downloads a file from a URL, reads its content, replaces headers and
- * domains using provided functions, and writes the modified content to an output file.
- * @param {string} url - The `url` parameter is a string representing the URL from which the file will
- * be downloaded.
- * @param {string} dest - The `dest` parameter in the `processFile` function represents the destination
- * path where the file will be downloaded to before processing. It is a string that specifies the
- * location on the file system where the downloaded file will be saved.
- * @param {string} outputFile - The `outputFile` parameter in the `processFile` function represents the
- * path where the processed file will be saved after applying the replacement functions. This parameter
- * specifies the location and name of the output file that will contain the modified content.
- * @param replaceHeaderFunc - The `replaceHeaderFunc` parameter is a function that takes a string as
- * input and returns a modified version of that string. In the `processFile` function, this function is
- * used to replace or modify the header content in the text read from a file before further processing.
- * @param replaceDomainFunc - The `replaceDomainFunc` parameter is a function that takes a string as
- * input and returns a modified version of that string where domain-related content has been replaced
- * or manipulated in some way. This function is used within the `processFile` function to process the
- * text content of a file before writing it to
- * @returns If an error occurs during the process of downloading, reading, or writing the file, the
- * function will return early and not proceed with further processing. If all operations are
- * successful, the function will complete without explicitly returning a value.
+ * The `processFile` function downloads a file from a URL, reads its content, modifies the data, and
+ * writes the modified data to a new file.
+ * @param {string} url - The `url` parameter in the `processFile` function represents the URL from
+ * which the file needs to be downloaded and processed. It is a string that contains the address of the
+ * file to be fetched.
+ * @param {string} tempFileName - The `tempFileName` parameter in the `processFile` function represents
+ * the name of the temporary file where the downloaded content will be stored before processing. This
+ * file will be used to read the data, modify it, and then write the processed content to another file.
+ * It acts as an intermediate storage location
+ * @param {string} processedFileName - The `processedFileName` parameter in the `processFile` function
+ * represents the name of the file where the processed data will be saved after modifying it. This file
+ * will be stored in the `rulesFolder` directory.
  */
 const processFile = async (
   url: string,
-  dest: string,
-  outputFile: string,
-  replaceHeaderFunc: (text: string) => string,
-  replaceDomainFunc: (text: string) => string,
+  tempFileName: string,
+  processedFileName: string,
 ) => {
   try {
-    await download(url, dest);
+    await downloadFile(url, join(tempFolder, tempFileName));
   } catch (error) {
     if (error instanceof Error) {
-      console.error(`Failed to download rules: ${error.message}`);
-      throw error;
+      throw new Error(`Failed to download file: ${error.message}`);
     }
-    return;
+
+    throw new Error('Failed to download file');
   }
 
-  let text: string;
+  let readData: string;
   try {
-    text = await fsPromises.readFile(dest, 'utf-8');
+    readData = await fsPromises.readFile(
+      join(tempFolder, tempFileName),
+      'utf-8',
+    );
   } catch (error) {
     if (error instanceof Error) {
-      console.error(`Failed to read file ${dest}: ${error.message}`);
-      throw error;
+      throw new Error(`Failed to read file: ${error.message}`);
     }
-    return;
+
+    throw new Error('Failed to read file');
   }
 
-  const header = replaceHeaderFunc(text);
-  const domains = replaceDomainFunc(header);
+  const modifiedHeaderData = modifyHeader(readData);
+  const modifiedDomainData = modifyDomain(modifiedHeaderData);
 
   try {
-    await fsPromises.writeFile(outputFile, domains);
+    await fsPromises.writeFile(
+      join(rulesFolder, processedFileName),
+      modifiedDomainData,
+    );
   } catch (error) {
     if (error instanceof Error) {
-      console.error(`Failed to write file ${outputFile}: ${error.message}`);
-      throw error;
+      throw new Error(`Failed to write file: ${error.message}`);
     }
+
+    throw new Error('Failed to write file');
   }
+
+  console.log(`Successfully processed ${url}`);
 };
 
 const main = async () => {
@@ -185,48 +262,18 @@ const main = async () => {
     await ensureDirectoryExists(tempFolder);
     await ensureDirectoryExists(rulesFolder);
 
-    const antiAdsD3wardTemp = join(tempFolder, 'd3ward.txt');
-    const antiAdsTemp = join(tempFolder, 'oisd_full_abp.txt');
-    const antiNSFWTemp = join(tempFolder, 'oisd_nsfw_abp.txt');
-
-    const antiAdsD3wardProcessed = join(
-      rulesFolder,
-      'Blocklist_Ads_D3ward.yaml',
-    );
-    const antiAdsProcessed = join(rulesFolder, 'Blocklist_Ads.yaml');
-    const antiNSFWProcessed = join(rulesFolder, 'Blocklist_NSFW.yaml');
-
-    await Promise.all([
-      processFile(
-        antiAdsD3wardUrl,
-        antiAdsD3wardTemp,
-        antiAdsD3wardProcessed,
-        replaceHeaderD3ward,
-        replaceDomainD3ward,
-      ),
-      processFile(
-        antiAdsUrl,
-        antiAdsTemp,
-        antiAdsProcessed,
-        replaceHeader,
-        replaceDomain,
-      ),
-      processFile(
-        antiNSFWUrl,
-        antiNSFWTemp,
-        antiNSFWProcessed,
-        replaceHeader,
-        replaceDomain,
-      ),
-    ]);
+    for (const { url, tempFileName, processedFileName } of rawUrls) {
+      await processFile(url, tempFileName, processedFileName);
+    }
 
     await fsPromises.rm(tempFolder, { recursive: true });
+    console.log('All files processed successfully');
   } catch (error) {
     if (error instanceof Error) {
-      console.error(`Failed to run the script: ${error.message}`);
-    } else {
-      console.error('Failed to run the script');
+      throw new Error(`An error occurred: ${error.message}`);
     }
+
+    throw new Error('An error occurred');
   }
 };
 
